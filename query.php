baa3c797252782c052610f4e08f7c7ec8bff17a0
BlueribbonCNX <107038399+BlueribbonCNX@users.noreply.github.com>
2022-06-08 15:45:04 +0700

    Add files via upload

--- query.php
+++ query.php
@@ -6,7 +6,7 @@ switch($_GET['type']){
 	
 	case 'registered':
 
-		$u_id = $_GET['u_id'];
+		$user_id = $_GET['user_id'];
 		$sql = 'SELECT * FROM customer WHERE c_id = "1" limit 1';
 		$result = $mysqli->query($sql);
 		$row = $result->fetch_array(MYSQLI_ASSOC);
